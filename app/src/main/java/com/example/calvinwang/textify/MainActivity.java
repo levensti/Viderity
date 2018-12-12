a5ebe03e0fd73aa6ca16e06d3382196d46752b93
Steven Li <levensti@gmail.com>
2018-12-11 19:38:01 -0600

    Implementing Firebase VisionText.

--- app/src/main/java/com/example/calvinwang/textify/MainActivity.java
+++ app/src/main/java/com/example/calvinwang/textify/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
